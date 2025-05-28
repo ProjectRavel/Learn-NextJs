890f29b9332d6084c4f9274e81ccd8f5c4b9ca35
ProjectRavel <rafaelcodeid@gmail.com>
2025-05-28 23:11:32 +0700

    Add blog page components for first, second, and main blog

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,7 @@
+export default function FirstBlog() {
+    return (
+        <div>
+            <h1>This is the First Blog Page</h1>
+        </div>
+    );
+}
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,7 @@
+export default function SecondBlog() {
+    return (
+        <div>
+            <h1>This is the Second Blog Page</h1>
+        </div>
+    )
+}
