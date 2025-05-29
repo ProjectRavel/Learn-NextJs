8491d91e724f828c3888fb167db93c064d4329a8
ProjectRavel <rafaelcodeid@gmail.com>
2025-05-29 20:50:59 +0700

    Add About and Blog components with metadata

---
+++ src/app/(learn)/about/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'About Page',
+    description: 'This is the about page of the application.',
+}
+
+export default function About() {
+    return (
+        <div>
+            <h1>This About Page</h1>
+        </div>
+    )
+}
