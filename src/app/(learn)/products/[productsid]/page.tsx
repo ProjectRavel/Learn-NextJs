b891375751da78fbe095cf3bdebabed4773d1d84
ProjectRavel <rafaelcodeid@gmail.com>
2025-05-29 23:15:54 +0700

    Refactor product detail page to include metadata generation and improve structure

--- src/app/(learn)/products/[productsid]/page.tsx
+++ src/app/(learn)/products/[productsid]/page.tsx
@@ -1,18 +1,45 @@
-import { notFound } from "next/navigation";
-export default async function ProductDetail(
-  { params }: { params: { productsid: string } }  
-) {
-  const productsid = params.productsid;
+  import { notFound } from "next/navigation";
+  import { Metadata } from "next";
 
-  if (parseInt(productsid) > 1000) {
-    return notFound();
+  type Props = {
+      params: {
+          productsid: string;
+      };
   }
 
-  return (
-    <>
-      <h1>Product Detail:</h1>
-      <h2>Product ID: {productsid}</h2>
-      <p>Details about product {productsid} will be displayed here.</p>
-    </>
-  );
-}
+  export const generateMetadata = async ({
+      params,
+  }: Props): Promise<Metadata> => {
+      const productsid = params.productsid;
+      const title = await new Promise((resolve) => {
+          setTimeout(() => {
+              resolve(`Product ${productsid} Details`);
+          }, 5000);
+      });
+      const desctiption = await new Promise((resolve) => {
+          setTimeout(() => {
+              resolve(`Details about product ${productsid}`);
+          }, 5000);
+      })
+      return {
+        title: `${title}`,
+        description: `${desctiption}`,
+      };
+    };
+
+  export default async function ProductDetail({ params }: { params: { productsid: string } }  
+  ) {
+    const productsid = params.productsid;
+
+    if (parseInt(productsid) > 1000) {
+      return notFound();
+    }
+
+    return (
+      <>
+        <h1>Product Detail:</h1>
+        <h2>Product ID: {productsid}</h2>
+        <p>Details about product {productsid} will be displayed here.</p>
+      </>
+    );
+  }
