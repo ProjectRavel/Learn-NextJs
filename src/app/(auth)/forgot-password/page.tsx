1bf38ea78f14edbd0447926fa3e6c31f30dcd7dc
ProjectRavel <rafaelcodeid@gmail.com>
2025-05-29 20:49:53 +0700

    Add placeholder components for Forgot Password, Login, and Register pages

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPassword() {
+    return (
+        <div className="flex h-full w-full items-center justify-center">
+            <h1 className="text-2xl font-bold">Forgot Password Placeholder</h1>
+        </div>
+    );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+export default function Register() {
+    return (
+        <div className="flex h-full w-full items-center justify-center">
+            <h1 className="text-2xl font-bold">Register Placeholder</h1>
+        </div>
+    );
+}
